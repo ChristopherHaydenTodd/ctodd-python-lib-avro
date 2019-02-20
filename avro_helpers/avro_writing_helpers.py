a6637cd20d419ab9647ade7214cab22c876ee146
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-20 09:51:36 -0500

    adding lib to aid in the task of writing .avro files

---
+++ avro_helpers/avro_writing_helpers.py
@@ -0,0 +1,14 @@
+"""
+    Purpose:
+        Avro Writing Helpers.
+
+        This library is used to aid in the task of writing .avro files
+"""
+
+# Python Library Imports
+import logging
+
+
+###
+# Writing Helpers
+###
