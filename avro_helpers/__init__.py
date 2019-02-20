cbc63991cf76e38aa5c124c700842846e5bb7660
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-20 10:24:41 -0500

    adding new files to the __init__

--- avro_helpers/__init__.py
+++ avro_helpers/__init__.py
@@ -3,6 +3,8 @@
         Add Libraries to Path for Pip Installing
 """
 
+from .avro_exceptions import *
 from .avro_general_helpers import *
 from .avro_reading_helpers import *
+from .avro_schema_helpers import *
 from .avro_writing_helpers import *
