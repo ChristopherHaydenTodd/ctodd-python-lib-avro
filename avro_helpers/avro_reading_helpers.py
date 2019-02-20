49a24bd7b18c9e8b11c6b56c6e29b986b5490467
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-20 09:51:15 -0500

    adding lib to aid in the task of reading .avro files

---
+++ avro_helpers/avro_reading_helpers.py
@@ -0,0 +1,16 @@
+"""
+    Purpose:
+        Avro Reading Helpers.
+
+        This library is used to aid in the task of reading .avro files
+"""
+
+# Python Library Imports
+import logging
+from avro.datafile import DataFileReader
+from avro.io import DatumReader
+
+
+###
+# Reading Helpers
+###
