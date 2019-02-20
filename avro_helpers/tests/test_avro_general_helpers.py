39ee562bd5764729292fd4ef4af69ad18282dc0c
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-20 09:51:58 -0500

    adding tests directory and a base test file for each lib

---
+++ avro_helpers/tests/test_avro_general_helpers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Purpose:
+        Test File for avro_general_helpers.py
+"""
+
+# Python Library Imports
+import os
+import sys
+import pytest
+from unittest import mock
+
+# Import File to Test
+from avro_helpers import avro_general_helpers
+
+
+###
+# Fixtures
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Mocked Functions
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Test Payload
+###
+
+
+# None at the Moment (Empty Test Suite)
